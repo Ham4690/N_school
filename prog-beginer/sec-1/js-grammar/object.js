86fa36c13e7ce473c45e1f3d7097d2d47ec51b9e
arata <ham46903@gmail.com>
2020-10-08 15:36:59 +0900

    fin 1-12!

--- prog-beginer/sec-1/js-grammar/object.js
+++ prog-beginer/sec-1/js-grammar/object.js
@@ -1,14 +1,22 @@
-var startTime = null;
+var game = {
+    startTime: null,
+    displayArea: document.getElementById('display-area')
+};
 function start() {
-    startTime = Date.now();
-    document.body.onkeydown = function() {
-        console.log('Stop!!');
-    }
+    game.startTime = Date.now();
+    document.body.onkeydown = stop;
     console.log('Start!!');
 }
 
 function stop() {
-
+    var currentTime = Date.now();
+    var seconds = (currentTime - game.startTime) / 1000;
+    if (9.5 <= seconds && seconds <= 10.5) {
+        game.displayArea.innerText = seconds + 'sec でした。すごい!!';
+    } else {
+        game.displayArea.innerText = seconds + 'sec でした。残念....';
+    }
+    console.log('Stop!!');
 }
 
 if (confirm('OKをおして10秒だと思ったら何かキーをおしてください。')){
